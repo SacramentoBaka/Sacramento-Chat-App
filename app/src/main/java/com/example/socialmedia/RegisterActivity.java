eb5f5af2e0af825a8e3aa1965bd1acc8c55d3ce6
Sacramento <sacramentomakhubele@gmail.com>
2023-11-03 21:31:20 +0200

    Registering user is done...

--- app/src/main/java/com/example/socialmedia/RegisterActivity.java
+++ app/src/main/java/com/example/socialmedia/RegisterActivity.java
@@ -1,14 +1,111 @@
 package com.example.socialmedia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText email, password, confirmPass;
+    private Button registerBTN, loginBTN;
+    private CheckBox checkBox;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        email = findViewById(R.id.idRegisterEmail);
+        password = findViewById(R.id.idRegisterPassword);
+        confirmPass = findViewById(R.id.idRegisterConfirmPassword);
+        progressBar = findViewById(R.id.idRegisterProgressBar);
+        checkBox = findViewById(R.id.idRegisterCheckBox);
+        mAuth = FirebaseAuth.getInstance();
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    confirmPass.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }else {
+                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    confirmPass.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+        registerBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailText = email.getText().toString();
+                String passText = password.getText().toString();
+                String confirmPassText = confirmPass.getText().toString();
+
+                if(!TextUtils.isEmpty(emailText) || !TextUtils.isEmpty(passText) || !TextUtils.isEmpty(confirmPassText)){
+                    if(passText.equals(confirmPassText)){
+                        progressBar.setVisibility(View.VISIBLE);
+                        mAuth.createUserWithEmailAndPassword(emailText,passText).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    sendToMain();
+                                }else {
+                                    String error = task.getException().getMessage();
+                                    Toast.makeText(RegisterActivity.this, error, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }else {
+                        progressBar.setVisibility(View.INVISIBLE);
+                        Toast.makeText(RegisterActivity.this, "Password do not Match", Toast.LENGTH_SHORT).show();
+                    }
+                }else {
+                    progressBar.setVisibility(View.INVISIBLE);
+                    Toast.makeText(RegisterActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+                }
+                loginBTN.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+
+            }
+        });
+    }
+    private void sendToMain() {
+        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            sendToMain();
+        }
     }
 }
