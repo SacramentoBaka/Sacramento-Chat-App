b76a2c4aa717bba6d175b78a42912c0571c17e15
Sacramento <sacramentomakhubele@gmail.com>
2023-11-04 08:40:51 +0200

    Bottom NavigationView in MainActivity but not final...

--- app/src/main/java/com/example/socialmedia/MainActivity.java
+++ app/src/main/java/com/example/socialmedia/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.socialmedia;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -18,8 +22,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+        BottomNavigationView mainBottomNavigationView = findViewById(R.id.idMainBottomNavigation);
+        mainBottomNavigationView.setOnItemSelectedListener(onNav);
+        getSupportFragmentManager().beginTransaction().replace(R.id.idMainFrame, new HomeFragment()).commit();
+
+
     }
-    public void logOut(View view){
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNav = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selected = null;
+            switch (item.getItemId()) {
+                case R.id.idMenuHome:
+                    selected = new HomeFragment();
+                    break;
+                case R.id.idMenuProfile:
+                    selected = new ProfileFragment();
+                    break;
+                case R.id.idMenuAsk:
+                    selected = new AskFragment();
+                    break;
+                case R.id.idMenuQueue:
+                    selected = new PeopleFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.idMainFrame, selected).commit();
+
+            return true;
+        }
+    };
+
+    public void logOut(View view) {
         mAuth.signOut();
         Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
         startActivity(intent);
@@ -30,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if(user == null){
+        if (user == null) {
             Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
             startActivity(intent);
             finish();
